002c48b8ed5387b039958dc891e98be9d42db218
Tina Mischke <tmischke999@gmail.com>
2024-11-21 10:18:28 -0600

    Reapply .gitattributes and normalize line endings

--- dist/tests/my-first-test.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const test_1 = require("@playwright/test");
-(0, test_1.test)('My first test', async ({ page }) => {
-    await page.goto('https://linkedin.com');
-    await (0, test_1.expect)(page).toHaveTitle('LinkedIn: Log In or Sign Up'); // Updated for LinkedIn
-});
--- tests/examples/my-first-test.spec.ts
+++
@@ -1,8 +0,0 @@
-export {};
-
-import { test, expect, Page } from '@playwright/test';
-
-test('My first test', async ({ page }: { page: Page }) => {
-  await page.goto('https://linkedin.com');
-  await expect(page).toHaveTitle('LinkedIn: Log In or Sign Up'); // Updated for LinkedIn
-});
